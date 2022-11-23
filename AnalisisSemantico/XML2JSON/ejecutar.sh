758243ddea83e713c4f0e4295f61dd6ba32edd4f
joseantoniols1212 <joseantonioluquesalguero1212@gmail.com>
2022-11-23 21:05:09 +0100

    xml2json

---
+++ AnalisisSemantico/XML2JSON/ejecutar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+name="$(basename "$PWD")"
+
+# Compilamos los archivos
+jflex $name.flex
+cup $name.cup
+javac -cp ../LibreriaCup/java-cup-11b-runtime.jar: *.java
+
+# Ejecutamos las pruebas
+for file in pruebas/*
+do
+  echo " ####### $file #######"
+  java -cp ../LibreriaCup/java-cup-11b-runtime.jar: $name $file
+  # echo " -- Comprobacion con el resultado esperado : "
+done
+
+rm Yylex.* sym.* parser* $name.class
